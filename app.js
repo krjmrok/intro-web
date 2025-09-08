9ede021a6d757542d05ceb0ecd2dcc6069a8517e
krjmrok <kriku2001@gmail.com>
2025-09-08 22:22:22 +0900

    app.jsの修正

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-const express = require("express");
+
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2');
+
 const app = express();
+const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Express!");
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
-app.listen(3000, () => {
+connection.connect((err) => {
+  if (err) {
+    console.error('DB接続失敗:', err);
+    return;
+  }
+  console.log('MySQLに接続成功！');
+});
+
+app.get('/', (req, res) => {
+  connection.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      res.status(500).send('クエリ失敗');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.listen(port, () => {
   console.log("http://localhost:3000 でサーバー起動中");
-});
-
-app.get("/top", (req, res) => {
-  res.render(`index.html`);
 });
