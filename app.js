924cce699eb5839bd26ba81c9e05cb4c6bd651d7
krjmrok <kriku2001@gmail.com>
2025-09-17 22:28:35 +0900

    HTML/CSS作成準備

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ connection.connect((err) => {
   console.log('MySQLに接続成功！');
 });
 
-app.get('/', (req, res) => {
+app.use(express.static('public'));
+
+app.get('/users', (req, res) => {
   connection.query('SELECT * FROM users', (err, results) => {
     if (err) {
       res.status(500).send('クエリ失敗');
